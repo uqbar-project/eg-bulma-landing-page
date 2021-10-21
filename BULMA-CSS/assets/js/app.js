126149f4896108f492cf62013b8fdbf4f4bc0df4
Jorge <lescanojorgeluis@gmail.com>
2021-10-21 02:35:51 -0300

    Bulma Css Nav

--- BULMA-CSS/assets/js/app.js
+++ BULMA-CSS/assets/js/app.js
@@ -1,32 +1,8 @@
-// scroll to top functionality
-const scrollUp = document.querySelector("#scroll-up");
-
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  const boton = document.querySelector(".navbar-burger");
+  const menu = document.querySelector(".navbar-menu");
+  boton.onclick = () => {
+    menu.classList.toggle("is-active");
+    boton.classList.toggle("is-active");
+  };
 });
-
-
-// Nav hamburgerburger selections
-
-const burger = document.querySelector("#burger-menu");
-const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
-
-burger.addEventListener("click", () => {
-  ul.classList.toggle("show");
-});
-
-// Close hamburger menu when a link is clicked
-
-// Select nav links
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach((link) =>
-  link.addEventListener("click", () => {
-    ul.classList.remove("show");
-  })
-);
