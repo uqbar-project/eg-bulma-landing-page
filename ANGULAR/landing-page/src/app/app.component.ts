8cb923080edb13eff8600e9b0577bead00fb621f
Jorge <lescanojorgeluis@gmail.com>
2021-10-21 18:06:20 -0300

    Bulma Css with Angular

--- ANGULAR/landing-page/src/app/app.component.ts
+++ ANGULAR/landing-page/src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'landing-page';
+  logos_1 = ['assets/icons/icons8-html-5.svg', 'assets/icons/icons8-css3.svg', 'assets/icons/icons8-javascript.svg'];
+  logos_2 = ['assets/icons/icons8-bootstrap.svg', 'assets/icons/icons8-react-native.svg', 'assets/icons/icons8-git.svg'];
+  social_icons = [
+    { name:'assets/icons/icons8-twitter-circled.gif', value_alt:'Twitter',}, 
+    { name:'assets/icons/icons8-instagram.gif', value_alt:'Instagram',},
+    { name:'assets/icons/icons8-linkedin-circled.gif', value_alt:'Linkedin',},
+    { name:'assets/icons/icons8-github.gif', value_alt:'Github',}
+  ];
+  nav_text = ['Nosotros','Habilidades','Proyectos','Contacto'];
+  projects_data = [
+    {
+      name: 'Adidas Sports',
+      value_alt:'adidas-sports',
+      path: 'assets/images/adidas.JPG'
+    },
+    {
+      name: 'Nike Experience',
+      value_alt:'nike-expeience',
+      path: 'assets/images/nike.JPG'
+    },
+    {
+      name: 'Puma Runners',
+      value_alt:'puma-runners',
+      path: 'assets/images/puma.JPG'
+    }
+  ];
 }
---
+++ ANGULAR/landing-page/src/assets/js/app.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const boton = document.querySelector(".navbar-burger");
+  const menu = document.querySelector(".navbar-menu");
+  boton.onclick = () => {
+    menu.classList.toggle("is-active");
+    boton.classList.toggle("is-active");
+  };
+});
+
+/* const scrollUp = document.querySelector("#scroll-up");
+
+console.log(scrollUp);
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+}); */
